7246cafde753b130370cda754dcdb0c4728255f0
wplearnerz <75932964+wplearnerz@users.noreply.github.com>
2024-06-20 05:19:28 -0700

    Update App.tsx
    resolved some errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  function App() {
+    
+    return (
+  
     <>
       <h1 className='mb-4'>Hello World</h1>
       <p>Muhammad Azhar</p>
     </>
-  )
-}
-
-export default App
+    )
+  }
+    export default App
